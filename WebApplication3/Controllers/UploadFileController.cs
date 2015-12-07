c5d569dbbbb719bae3d0b2ecee1fb816e6a8c97c
Skupina7 <skupina7music@gmail.com>
2015-12-07 13:42:50 +0100

    FirstUpload

---
+++ WebApplication3/Controllers/UploadFileController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class UploadFileController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        private bool isValidContentType(string contentType)
+        {
+            return contentType.Equals("image/png") || contentType.Equals("image/gif") || contentType.Equals("image/jpg") || contentType.Equals("image/jpeg");
+        }
+
+        private bool isValidContentLenght(int contentLenght)
+        {
+            return ((contentLenght / 1024) / 1024) < 1; // 1MB
+        }
+        [HttpPost]
+        public ActionResult Process(HttpPostedFileBase photo)
+        {
+            if (!isValidContentType(photo.ContentType))
+            {
+                ViewBag.Error = "Only PNG, GIF, JPG and JPEG files are allowed.";
+                return View("Index");
+            }
+            else if (!isValidContentLenght(photo.ContentLength))
+            {
+                ViewBag.Error = "Max file size is 1MB.";
+                return View("Index");
+            }
+            else
+            {
+                if(photo.ContentLength > 0)
+                {
+                    var fileName = Path.GetFileName(photo.FileName);
+                    var path = Path.Combine(Server.MapPath("~/content/images"), fileName);
+                    photo.SaveAs(path);
+                    ViewBag.fileName = photo.FileName;
+                }
+                return View("Success");
+            }
+            
+        }
+    }
+}
