1c13afc71368880137092497cfa9c1bb067e08bd
Skupina7 <skupina7music@gmail.com>
2016-01-11 11:21:59 +0100

    Dodanih par stvar

---
+++ WebApplication3/WebApplication3/Controllers/ImagesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ImageManager.Models;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class ImagesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Images
+        public ActionResult Index()
+        {
+            return View(db.Images.ToList());
+        }
+
+        // GET: Images/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Image Image = db.Images.Find(id);
+            if (Image == null)
+            {
+                return HttpNotFound();
+            }
+            return View(Image);
+        }
+
+        // GET: Images/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Images/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ImageId,Name,Address,City,State,Zip,Email")] Image Image)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Images.Add(Image);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(Image);
+        }
+
+        // GET: Images/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Image Image = db.Images.Find(id);
+            if (Image == null)
+            {
+                return HttpNotFound();
+            }
+            return View(Image);
+        }
+
+        // POST: Images/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ImageId,Name,Address,City,State,Zip,Email")] Image Image)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(Image).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(Image);
+        }
+
+        // GET: Images/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Image Image = db.Images.Find(id);
+            if (Image == null)
+            {
+                return HttpNotFound();
+            }
+            return View(Image);
+        }
+
+        // POST: Images/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Image Image = db.Images.Find(id);
+            db.Images.Remove(Image);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
